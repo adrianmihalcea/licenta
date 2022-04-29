78deacf27a54e06e1bb142408002d18334c02a6d
Adrian Mihalcea <amihai.mihalcea@gmail.com>
2022-04-29 12:37:22 +0300

    Alchemy mint and view NFT

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const pinataSDK = require('@pinata/sdk');
+const fs = require('fs');
 
 const secrets = require('./secrets');
 const constants = require('./constants')
+const alchemyWrappers = require('./alchemy_calls');
 
 const app = express();
 const port = 8080;
@@ -14,7 +16,13 @@ app.listen(port, () => {
     console.log('Starting up Backend on port 8080');
 });
 
+app.get('/view', (req, res) => {
+    alchemyWrappers.view_nft_metadata('4');
+    return res.send('got\'em');
+});
+
 app.get('/test', (req, res) => {
+    alchemyWrappers.mint('testMintNou');
     return res.send({'hello': 'world'})
 });
 
@@ -28,7 +36,7 @@ app.get('/testPinata', (req, res) => {
     });
 
     return res.send('nice');
-})
+});
 
 app.post('/upload', (req, res) => {
     if (req.files === null) {
@@ -45,5 +53,42 @@ app.post('/upload', (req, res) => {
         return res.status(500).send(err);
         }
         return res.send({ status: "success", path: path });
+    });
+});
+
+app.get('/pinJson', (req, res) => {
+    const body = {
+        lorem: 'ipsum'
+    }
+
+    const options = {
+        pinataMetadata: {
+            name: 'test.json'
+        }
+    }
+
+    pinata.pinJSONToIPFS(body, options).then((result) => {
+        console.log(result);
+        res.send('all gucci');
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send(err);
+    });
+});
+
+app.get('/pinFile', (req, res) => {
+    const readableStreamForFile = fs.createReadStream('./uploads/1651183001594current.txt')
+    const options = {
+        pinataMetadata: {
+            name: '1651183001594current.txt'
+        }
+    }
+
+    pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
+        console.log(result);
+        res.send('all gucci');
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send(err);
     });
 });
---
+++ backend/view.js
@@ -0,0 +1,9 @@
+// const axios = require('axios');
+
+// const view_all = (req, res) => {
+
+// };
+
+// module.exports = {
+//     view_all: view_all
+// }
