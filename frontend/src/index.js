a43a36e431cadf7c9b44609b27e3dbc0f9ae2a90
Adrian Mihalcea <amihai.mihalcea@gmail.com>
2022-06-26 15:57:51 +0300

    Include Backend tests + repo cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,23 +14,6 @@ root.render(
     <App />
 
   </Router>,
-
-  // document.querySelector('#root')
-
 )
-// ReactDOM.render(
-//   <>
-//     {/* <Router><App /></Router> */}
-//     <App></App>
-//   </>,
-//   document.getElementById('root')
-  
-// );
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   < Router >
-//     <App />
-//   </Router >
-// );
 
 reportWebVitals();
